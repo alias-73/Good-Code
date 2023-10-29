4f35b3be31d651f84df07a074e77638d796ea2e1
Seyed Hossein Sadegh Zade <seyedhossein.sz@outlook.com>
2023-10-29 20:12:45 +0330

    first commit

---
+++ GooCode.cs
@@ -0,0 +1,39 @@
+ 
+ 
+ // TextBox : محدود کردن به حروف عددی
+ private void NumericTextBox_KeyPress(object sender, KeyPressEventArgs e)
+ {
+     // Check if the pressed key is a digit, Backspace, or Delete
+     if (!char.IsDigit(e.KeyChar) && e.KeyChar != '\b' && e.KeyChar != (char)Keys.Delete)
+     {
+         e.Handled = true; // Ignore the key press
+     }
+ }
+
+// TextBox : جدا کردن 3 رقم 3 رقم 
+   private void FormatAndSelectText(TextBox textBox)
+  {
+      if (string.IsNullOrEmpty(textBox.Text) || textBox.Text == "0")
+          return;
+
+      long value;
+      if (long.TryParse(textBox.Text.Replace(",", ""), out value))
+      {
+          textBox.Text = value.ToString("#,0");
+          textBox.Select(textBox.TextLength, 0);
+      }
+  }
+
+
+//Label : جدا کردن 3 رقم 3 رقم
+  private void FormatLabelText(Label label)
+  {
+      if (string.IsNullOrEmpty(label.Text) || label.Text == "0")
+          return;
+
+      long value;
+      if (long.TryParse(label.Text.Replace(",", ""), out value))
+      {
+          label.Text = value.ToString("#,0");
+      }
+  }
